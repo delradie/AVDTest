23274363051657f59f61de34dec6028874490f85
Mark Middlemist <mark.middlemist@mercatosolutions.co.uk>
2023-12-14 12:21:53 +0000

    Adding test project

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace AVDTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonF2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.buttonF2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(976, 607);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonF2
+            // 
+            this.buttonF2.BackColor = System.Drawing.Color.RosyBrown;
+            this.buttonF2.Location = new System.Drawing.Point(8, 8);
+            this.buttonF2.Name = "buttonF2";
+            this.buttonF2.Size = new System.Drawing.Size(99, 47);
+            this.buttonF2.TabIndex = 0;
+            this.buttonF2.Text = "Open";
+            this.buttonF2.UseVisualStyleBackColor = false;
+            this.buttonF2.Click += new System.EventHandler(this.buttonF2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(996, 627);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonF2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AVDTest
+{
+    public partial class Form1 : Form
+    {
+        Form2 TestForm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonF2_Click(object sender, EventArgs e)
+        {
+            if(TestForm != null && TestForm.Visible) 
+            { 
+                TestForm.BringToFront();
+            }
+            else
+            {
+                TestForm = new Form2();
+                TestForm.Show(this);
+            }
+        }
+    }
+}
